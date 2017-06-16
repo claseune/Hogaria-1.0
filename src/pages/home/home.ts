abc2851c729974244edc500421f37b6bd9bb8dc8
Hector Antonio velazquez Gonzalez <havg.1997@hotmail.com>
2017-06-16 00:44:05 -0500

    Add files via upload
    Practica de Guardado de Datos con firebase + avance

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,18 +1,33 @@
 import { Component } from '@angular/core';
-import { NavController,NavParams, ToastController, ModalController } from 'ionic-angular';
+import { NavController, NavParams, ToastController, ModalController, AlertController } from 'ionic-angular';
 import { HogariaPg2Page } from '../hogaria-pg2/hogaria-pg2';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import * as firebase from 'firebase';
-import {PerfilPage} from '../perfil/perfil';
+import { PerfilPage } from '../perfil/perfil';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
   private data: FormGroup;
+  private formexdata: FormGroup;
+
+  //nuevo
+  info: any;
+  records: FirebaseListObservable<any>;
 
   constructor(public navCtrl: NavController, public formBuilder: FormBuilder,
-  public toastCtrl: ToastController, public modalCtrl: ModalController) {
+    public toastCtrl: ToastController, public modalCtrl: ModalController, public alertCtrl: AlertController, db: AngularFireDatabase) {
+    //nuevo
+    this.records = db.list('/usuarios');
+    this.formexdata = this.formBuilder.group({
+
+      name: ['', Validators.required],
+      gender: ['', Validators.required],
+      hobby: ['', Validators.required]
+    })
+
     this.data = this.formBuilder.group
       ({
         username: ['', Validators.required],
@@ -21,10 +36,13 @@ export class HomePage {
 
   }
 
-Perfilar(){
+
+
+
+  Perfilar() {
     this.navCtrl.push(PerfilPage);
   }
-public showToast(text, time) {
+  public showToast(text, time) {
     const toast = this.toastCtrl.create({
       message: text,
       duration: time
@@ -32,11 +50,12 @@ public showToast(text, time) {
     toast.present();
   };
 
+
   nextcambio() {
     this.navCtrl.push(HogariaPg2Page);
   }
 
-  
+
   login() {
     firebase.auth().signInWithEmailAndPassword(this.data.controls['username'].value, this.data.controls['password'].value)
       .then((response) => {
@@ -47,7 +66,6 @@ public showToast(text, time) {
         })
         console.log(response)
         this.navCtrl.setRoot(HomePage);
-
       })
       .catch((error) => {
         console.log(error.message)
@@ -56,7 +74,17 @@ public showToast(text, time) {
   register() {
     this.modalCtrl.create(HogariaPg2Page).present();
   }
+  guardarForm() {
+    if (this.formexdata.valid) {
+      this.records.push(this.formexdata.value)
+    }
+    else {
+      console.error('Verifique su informacion')
+    }
+  }
+
   ionViewDidLoad() {
+    console.log(this.records)
 
   }
 
--- src/pages/perfil/perfil.ts
+++ src/pages/perfil/perfil.ts
@@ -1,25 +1,48 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 //nose si haya que dejar esto
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import {HomePage} from '../home/home'
 /**
  * Generated class for the PerfilPage page.
  *
  * See http://ionicframework.com/docs/components/#navigation for more info
  * on Ionic pages and navigation.
  */
-@IonicPage()
+
 @Component({
   selector: 'page-perfil',
   templateUrl: 'perfil.html',
 })
 export class PerfilPage {
 
+slides = [
+    {
+      title: "Bienvenido a Hogaria!",
+      description: "Te ayudamos a <b>localizar esa casa o departamento que deseas rentar o comprar </b>  <p> Con tu ubicacion la app te mostrarà las mejores opciones cerca de ti, o puedes usar el mapa para moverte por zonas y encontrar tu hogar ideal.</p>",
+      image: "assets/img/hgmanos.jpg",
+    },
+    {
+      title: "¿Eres un Vendedor?",
+      description: "<b>Has encontrado tu mejor herramienta!</b> <p>Con <b>Hogaria</b> podras publicar esa casa o departamento que quieres rentar o vender </p><b>Te ayudaremos a crear la mejor publicidad a tu hogar</b></p> .</p>",
+      image: "assets/img/agente.jpg",
+    },
+    {
+      title: "Google Maps",
+      description: "Gracias a <b>Google Maps</b> podras saber que hay cerca de el hogar que encontraste, asi podras determinar cual es tu mejor opciòn.",
+      image: "assets/img/maps.gif",
+    }
+  ];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+  Login(){
+    this.navCtrl.push(HomePage);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PerfilPage');
   }
+
+
 
 }
